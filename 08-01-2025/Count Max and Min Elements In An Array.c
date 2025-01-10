f5e46aba758620a98ea35c8f0ec6c95bdcc6bd71
AnanduSaseendren <anandusaseendrenkm@gmail.com>
2025-01-10 10:11:41 +0530

    Update Count Max and Min Elements In An Array.c

--- 08-01-2025/Count Max and Min Elements In An Array.c
+++ 08-01-2025/Count Max and Min Elements In An Array.c
@@ -8,19 +8,20 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 #include <stdio.h>
 
-int main()
+int main() 
 {
-    int main() {
     int arr[] = {10, 5, 8, 20, 15, 2, 30};  
     int n = sizeof(arr) / sizeof(arr[0]);
     
     int max = arr[0], min = arr[0];
 
     for (int i = 1; i < n; i++) {
-        if (arr[i] > max)
+        if (arr[i] > max){
             max = arr[i];
-        if (arr[i] < min)
+        }
+        if (arr[i] < min){
             min = arr[i];
+        }
     }
 
     printf("Maximum element: %d\n", max);
